864495a02366c99c7d070ef177becf4846819810
yongshangmoli <yongshangmoli@users.noreply.github.com>
2016-07-12 10:39:30 +0800

    Update common.js

--- sufferer/js/common.js
+++ sufferer/js/common.js
@@ -1,16 +1,24 @@
-// var suffererRecord = {
-// 	preDoctor : "Pei",
-// 	preSufferer : [],
-// 	curSufferer : {},
-// };
-// var utilG = {
-// 	nullCheck : function(text) {
-// 		return !(text === "" || text === null);
-// 	},
-// };
-//header返回按键
-$("#goback").on("click",function(e) {
-	console.log(11);
-	window.history.go(-1);
-});
+function alert_msg(msg,className){
+  var className = className || 'alert_tip';
+  function lock(e){e.preventDefault();}
+  var el = document.querySelector('.'+className);
+  if(!el) {
+    el = document.createElement('div');
+    document.body.appendChild(el);
+  }
+  else {
+  	el.style.display = "block";
+  }
+  document.addEventListener('touchstart',lock);
+  el.addEventListener('touchend',function(e){
+    var target = e.target;
+    if(target.tagName.toLowerCase() !== 'span') return;
+    el.style.display = "none";
+    document.removeEventListener('touchstart',lock);
+  });
+  el.innerHTML = '<span></span>'+msg || '';
+  el.className = className+' active';
+}
 
+window.Alert = alert;
+window.alert = alert_msg;
