9cdf03779dd6b1bf214495b43bda276cd33a1ca0
yongshangmoli <yongshangmoli@users.noreply.github.com>
2016-07-14 10:42:43 +0800

    Update common.js

--- sufferer/js/common.js
+++ sufferer/js/common.js
@@ -31,16 +31,16 @@ var lockScreen = {
 function lockScreenHgt() {
   lockScreen._page.style.height = lockScreen._windowH + "px";
   lockScreen._page.style.overflow = "hidden";
-  if(lockScreen._subBtn) {
+ /* if(lockScreen._subBtn) {
     lockScreen._subBtn.style.position = "static";
-  }
+  }*/
 }
 function unlockScreenHgt() {
   lockScreen._page.style.height = "auto";
   lockScreen._page.style.overflow = "auto";
-  if(lockScreen._subBtn) {
+  /*if(lockScreen._subBtn) {
     lockScreen._subBtn.style.position = "relative";
-  }
+  }*/
 }
 
 //loading
